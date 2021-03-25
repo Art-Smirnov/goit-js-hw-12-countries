6c385db2d19c38e9d04955677e0f0eb2fd88e105
Artem Smirnov <aratatem2013@gmail>
2021-03-25 18:22:37 +0200

    add fixes

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,12 +1,9 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
-  });
+  return fetch(`${BASE_URL}/name/${searchQuery}`).then(response =>
+    response.json(),
+  );
 }
 
 export default { fetchCountries };
